b7f6bfb7e6e5da1b549b6481da7f87db8ba60109
minhyeongKang <kang7937@gmail.com>
2026-05-30 16:33:49 +0900

    first commit

---
+++ src/main/java/com/hellomeen/cupongapplication/CupongapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.hellomeen.cupongapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupongapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupongapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellomeen/cupongapplication/CupongapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellomeen.cupongapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupongapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
